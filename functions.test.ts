b13f8e415ddb51860bd89f9ef6a80893d8e0c7fb
tayjanks <taylorjenkins801@gmail.com>
2022-11-09 20:36:02 -0700

    jest tests done

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,24 @@
 const {shuffleArray} = require('./utils')
+const fruit = [{id:1, name: 'Apple'},{id:2, name:'Pear'}]
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+test ("be an Array", () => {
+expect(shuffleArray(fruit)).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({id:1})
+      
+    ]))}
+  )
 })
+
+  test ("be the same length", () => 
+expect(shuffleArray(fruit).length).toEqual(fruit.length)
+  );
+
+
+
+
+
+
+
+
